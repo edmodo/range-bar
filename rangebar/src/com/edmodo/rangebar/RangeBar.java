f82585de40dd9acd4e9aae7e59cef659211f0a66
Oknesif <oknesif@gmail.com>
2014-10-30 20:13:39 +0300

    Getter and setters which allow to restore the state of view through java code.

--- rangebar/src/com/edmodo/rangebar/RangeBar.java
+++ rangebar/src/com/edmodo/rangebar/RangeBar.java
@@ -315,6 +315,38 @@ public class RangeBar extends View {
     }
 
     /**
+     * Set right index of the bar.
+     *
+     * @param rightIndex Integer specifying the right index value.
+     * @throws IllegalArgumentException throws then new value greater than count of ticks.
+     */
+    public void setRightIndex(int rightIndex) throws IllegalArgumentException {
+        if (rightIndex >= mTickCount) {
+            throw  new IllegalArgumentException("The value of the new index greater than count of ticks.");
+        } else {
+            this.mRightIndex = rightIndex;
+            setThumbIndices(mLeftIndex, mRightIndex);
+        }
+    }
+
+    /**
+     * Set left index of the bar.
+     *
+     * @param leftIndex Integer specifying the right index value.
+     */
+    public void setLeftIndex(int leftIndex) {
+        this.mLeftIndex = leftIndex;
+        setThumbIndices(mLeftIndex, mRightIndex);
+    }
+
+    /**
+     * @return Integer count of ticks.
+     */
+    public int getTickCount() {
+        return mTickCount;
+    }
+
+    /**
      * Sets the number of ticks in the RangeBar.
      * 
      * @param tickCount Integer specifying the number of ticks.
